34f8d4109bd5fed7b5959c0a6cc395bc1b6d6f75
Jung_Min <162419902+jammmin02@users.noreply.github.com>
2025-09-21 00:51:58 +0900

    Chore/core request response (#18)
    * response 작업
    
    * request class 작성

--- backend/app/Core/Response.php
+++ backend/app/Core/Response.php
@@ -1,2 +1,46 @@
 <?php
-// 표준 응답 포맷(success/data/error)
+// namespace App\Core;
+namespace Tripmate\Backend\Core;
+// 표준 응답 포멧 클레스
+class Response
+  {
+    // 성공 응답 출력 메서드
+    // status = 200 
+    public function success(array $data =[], int $status = 200): void
+    {
+      self::json([
+        'success' => true,
+        'data'=> $data
+      ], $status);
+    }
+    // 에러 응답 출력 메서드
+    // status = 400
+    // error => code, message 포함
+    public function error(string $code, string $message, int $status = 400) {
+      self::json([
+        'success' => false,
+        'error' => [
+          'code' => $code,
+          'message' => $message,
+        ],
+      ], $status);
+    }
+
+    private function json(array $payload, int $status): void
+    {
+      // HTTP 응답 코드 설정
+      http_response_code($status);
+      // JSON 응답 출력
+      // UTF-8 설정
+      header('Content-Type: application/json; charset=utf-8');
+      echo json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+      exit;
+
+    }
+
+  }
+
+
+?>
+
+
