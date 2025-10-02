f25bdbb74ee282b486135791c8e7b72b9d2eb1fe
dayeon2423004 <163419727+dayeon2423004@users.noreply.github.com>
2025-10-02 14:07:32 +0900

    feat:비밀번호 해쉬 처리 (#41)

--- backend/app/Common/Utils/Password.php
+++ backend/app/Common/Utils/Password.php
@@ -1,2 +1,24 @@
 <?php
-// 비밀번호 해시/검증
+    namespace Tripmate\Backend\Common\Utils;
+
+    // 해쉬
+    class Password {
+        // 비밀번호 해쉬
+        public static function PasswordHash($pwd) {
+            //비밀번호 해쉬
+            $hash_pwd = password_hash($pwd, PASSWORD_BCRYPT);
+
+            return $hash_pwd;
+        }
+
+        // 비밀번호 검증
+        public static function PasswordValdataion($pwd, $pwd_hash) {
+            // 비밀번호 검증
+            if(password_verify($pwd, $pwd_hash)) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+    }
