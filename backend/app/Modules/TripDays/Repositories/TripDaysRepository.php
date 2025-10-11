cab9736c03c335f329c936cfc6f6dbbdde9bc393
jammmin02 <jmkm1201@naver.com>
2025-10-11 20:59:19 +0900

    update(repository) :  transaction , rollback, commit 제어 메서드 작성

--- backend/app/Modules/TripDays/Repositories/TripDaysRepository.php
+++ backend/app/Modules/TripDays/Repositories/TripDaysRepository.php
@@ -1,1 +1,56 @@
 <?php
+// 1. 네임스페이스 선언
+namespace Tripmate\Backend\Modules\TripDays\Repositories;
+
+// 2. use 작성
+use Tripmate\Backend\Core\DB;
+use PDO;
+
+// 3. TripDaysRepository 클래스 정의
+class TripDaysRepository {
+  // 1. pdo 프러퍼티 정의
+  public PDO $pdo;
+
+  // 2. 생성자에서 DB 생성 및 접속 
+  public function __construct() {
+    // 2-1. DB 객체 생성
+    $db = new DB();
+    // 2-2. DB 접속
+    $this->pdo = $db->getConnection();
+  }
+
+  // 3. 트랜잭션 제어 메서드
+  public function beginTransaction() : bool {
+    // 3-1. 실패 시 false 반환
+    return $this->pdo->beginTransaction();
+  }
+
+  // 4. 커밋 제어 메서드
+  public function commit() : bool {
+    // 4-1. 트레젝션이 실행중인지 확인
+    if ($this->pdo->inTransaction()){
+      // 4-2. 실행중이라면 commit 실행
+      return $this->pdo->commit();
+    }
+    // 4-3. 실행중이 아니라면 false 반환
+    return false;
+  }
+
+  // 5. 롤백 제어 메서드
+  public function rollBack() : bool {
+    // 5-1. 트레젝션이 실행중인지 확인
+    if ($this->pdo->inTransaction()){
+      // 5-2. 실행중이라면 rollBack 실행
+      return $this->pdo->rollBack();
+    }
+    // 5-3. 실행중이 아니라면 false 반환
+    return false;
+  }
+
+  
+
+
+
+
+
+}    
