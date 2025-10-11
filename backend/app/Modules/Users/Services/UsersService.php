5a55adb98b708442eb207d8957029da271323a37
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-11 20:21:39 +0900

    UsersService 로직 작성

--- backend/app/Modules/Users/Services/UsersService.php
+++ backend/app/Modules/Users/Services/UsersService.php
@@ -1,1 +1,30 @@
 <?php
+    namespace Tripmate\Backend\Modules\Users\Services;
+    
+    use Tripmate\Backend\Modules\Users\Repositories\UsersReadRepository as Repository;
+
+    class UsersService {
+        public Repository $repository;
+
+        // 생성자 
+        public function __construct() {
+            // db 객체
+            $this->repository = new Repository();
+        }
+
+        // 내 정보 조회
+        public function UserMyPageService($user_id) {
+            //db 호출
+            $result = $this->repository->UserMyPageRepository($user_id);
+            
+            return $result;
+        }
+
+        // 회원 탈퇴
+        public function UserSecessionService($user_id) {
+            // DB에 전달
+            $result = $this->repository->UserSecessionRepository($user_id);
+        
+            return $result;
+        }
+    }
