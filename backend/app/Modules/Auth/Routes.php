2d9de862c7f24f3a058ee368fe9a233ba594bae3
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-11 18:38:33 +0900

    Rotes 함수명 및 경로 수정

--- backend/app/Modules/Auth/Routes.php
+++ backend/app/Modules/Auth/Routes.php
@@ -6,11 +6,11 @@
     // Auth 라우트 등록
     return function (\AltoRouter $router, Request $request, Response $response): void {
         // 회원가입 라우팅 등록
-        $router->map('POST', '/avi/v1/users', [new AuthController($request, $response), 'UserRegister']);
+        $router->map('POST', '/api/v1/users', [new AuthController($request, $response), 'userRegister']);
 
         // 로그인 라우팅 등록
-        $router->map('POST', '/avi/v1/auth/login', [new AuthController($request, $response), 'UserLogin']);
+        $router->map('POST', '/api/v1/auth/login', [new AuthController($request, $response), 'userLogin']);
 
         // 로그아웃 라우팅 등록
-        $router->map('POST', '/avi/v1/auth/logout', [new AuthController($request, $response), 'UserLogout']);
+        $router->map('POST', '/api/v1/auth/logout', [new AuthController($request, $response), 'userLogout']);
     };
