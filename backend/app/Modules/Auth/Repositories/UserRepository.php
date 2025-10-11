9de3d0b97245a3049be31005236f6df1c91b66ee
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-11 16:54:09 +0900

    feat: 로그인 및 로그아웃 기능 추가

--- backend/app/Modules/Auth/Repositories/UserRepository.php
+++ backend/app/Modules/Auth/Repositories/UserRepository.php
@@ -2,51 +2,89 @@
     namespace Tripmate\Backend\Modules\Auth\Repositories;
 
     use Tripmate\Backend\Core\DB;
+    use Tripmate\Backend\Common\Utils\Password;
 
     // DB 로직 작성
     class UserRepository {
         public DB $db;
+        public $pdo;
 
         public function __construct() {
             // DB 객체 생성
             $this->db = new DB();
+
+            // 함수 호출
+            $this->pdo = $this->db->getConnection();
         }
 
         // 회원가입 로직
         public function RegisterDB($email, $pwd_hash, $nickname) {
-            // DB 연결
-            $pdo = $this->db->getConnection();
-
             // 트레젝션
-            $pdo->beginTransaction();
+            $this->pdo->beginTransaction();
 
             // email 중복 검사
-            $result = $pdo->prepare("SELECT email_norm FROM Users WHERE email_norm = :email");
+            $result = $this->pdo->prepare("SELECT email_norm FROM Users WHERE email_norm = :email");
             if (!$result->execute(['email' => $email])) {
-                $pdo->rollback();
+                $this->pdo->rollback();
+                return "DB_EXCEPTION";
             }
 
             // 반환 값이 있는지 확인
             if ($result->fetch()) {
+                $this->pdo->rollback();
                 return "DUPLICATE_EMAIL";
             } 
 
             // 중복 값이 없을 경우 값 넣기
-            $insert = $pdo->prepare("INSERT INTO Users (email_norm, password_hash, name) VALUES (:email_norm, :password_hash, :name);");
+            $insert = $this->pdo->prepare("INSERT INTO Users (email_norm, password_hash, name) VALUES (:email_norm, :password_hash, :name);");
             
             if ($insert->execute(['email_norm' => $email, 'password_hash' => $pwd_hash, 'name' => $nickname])) {
                 // DB 처리 완료
-                $pdo->commit();
+                $this->pdo->commit();
 
                 // 성공 반환
                 return "REGISTER_SUCCESS";
 
             } else {
-                $pdo->rollback();
+                $this->pdo->rollback();
                 return "DB_ERROR";
             }
         }
         // 로그인 로직
+        public function LoginDB($email, $pwd) {
+            // 트레젝션 실행
+            $this->pdo->beginTransaction();
 
-        // 로그아웃 로직
+            // 이메일을 이용해 조회
+            $query = $this->pdo->prepare("SELECT user_id, password_hash FROM Users WHERE email_norm = ?;");
+            
+            if(!$query->execute([$email])) {
+                $this->pdo->rollback();
+                return "DB_EXCEPTION";
+            }
+
+            // 조회하기
+            $data = $query->fetch();
+
+            // 이메일 조회 반환 값이 없을 경우
+            if(!$data) {
+                $this->pdo->rollback();
+                return "AUTH_FAILED";
+            }
+
+            // 조회한 데이터 꺼내기
+            $user_id = $data['user_id'];
+            $pwd_hash = $data['password_hash'];
+
+            $this->pdo->commit();
+
+            // 비밀번호 검증
+            if(Password::PasswordValdataion($pwd, $pwd_hash)) {
+                // JWT 발급
+                return $user_id;
+            } else {
+                // 비밀번호가 알맞지 않을 경우
+                return "AUTH_FAILED";
+            }
+        }
     }
