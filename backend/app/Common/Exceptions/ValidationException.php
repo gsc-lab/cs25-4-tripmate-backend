bea74a1fd5169aeeb441a49b7600539f00466c9e
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-25 02:02:32 +0900

    ValidatorException 클래스 정의

---
+++ backend/app/Common/Exceptions/ValidationException.php
@@ -0,0 +1,14 @@
+<?php
+    namespace Tripmate\Backend\Core;
+
+    class ValidationException extends \Exception {
+        public array $errors;
+
+        public function __construct(array $errors, string $message = "잘못된 입력값입니다.", $code=400) {
+            // 부모 생성자로 메세지 전달
+            parent::__construct($message, $code);
+
+            // 에러 코드 전달
+            $this->errors = $errors;
+        }
+    }
