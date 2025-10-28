b97144ef1aa9c13e940947289647160e6aeaf2ec
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-29 02:17:31 +0900

    validator

--- backend/app/Core/Validator.php
+++ backend/app/Core/Validator.php
@@ -3,7 +3,7 @@
 
     // validator, NestedValidatonException, Response 라이브러리 가져와 사용
     use Respect\Validation\Validator as v;
-    use Respect\Validation\Exceptions\NestedvalidationException as nve;
+    use Respect\Validation\Exceptions\NestedValidationException as nve;
     use Tripmate\Backend\Common\Exceptions\ValidationException;
 
     // 클래스 정의
@@ -29,51 +29,20 @@
         }
 
         // Float타입 검사
-        private function vFloatRule() {
-            return v::floatVal()->notEmpty();
-        }
-
-        // 메세지와 맵 매핑 함수
-        private function mapErrors(array $messages, array $map) {
-            $errorMes = [];
-            
-            // 반복 매핑 
-            foreach ($map as $key => $code) {
-                if (isset($messages[$key])) {
-                    $errorMes[$key] = $code;
-                }
-            }
-            return $errorMes;
+        private function vRequiredFloat() {
+            return v::floatVal()->notOptional();
         }
 
         // try-catch 공통 함수
-        public function runValidate($validation, $data, $map) {
+        public function runValidate($validation, $data) {
             try {
                 // 검증 실행
                 $validation->assert($data);
             } catch (nve $e) {
-                // 모든 필드별 에러 메시지 배열
-                $messages = $e->getMessages();
-
-                // 매핑 함수 실행
-                $errorMes = $this->mapErrors($messages, $map);
-
-                // 에러발생 시키기
-                throw new ValidationException($errorMes);
+                // 모든 필드별 에러 메시지 반환
+                throw new ValidationException($e->getMessages());
             }
         }
-
-        // Parm 공통 함수
-        public function paramValidation($validation, $data, $errorMap) {
-            try {
-                // 검증 실행
-                $validation->assert($data);
-            } catch (nve $e) {
-                // 에러발생 시키기
-                throw new ValidationException($errorMap);
-            }
-        }
-
 
         // 로그인 유효성 검증
         public function validateUser(array $data) {
@@ -81,14 +50,8 @@
             $validation = v::key('email', v::email()->notEmpty()->length(null, 255), true)
                         -> key('password', v::alnum()->notEmpty()->length(8, 128), true);
 
-            // 에러 배열
-            $map = [
-                'email' => "EMAIL_INVALID",
-                'password' => "PASSWORD_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         } 
 
         // 회원가입 유효성 검증
@@ -96,17 +59,11 @@
             // nickname 유효성 검증
             $validation = v::key('nickname', $this->vLengthRule(1, 50), true)
                         -> key('email', v::email()->notEmpty()->length(null, 255), true)
-                        -> key('password', v::alnum()->notEmpty()->length(8, 128), true);
-
-            // 에러 배열
-            $map = [
-                'nickname' => "NICKNAME_INVALID",
-                'email' => "EMAIL_INVALID",
-                'password' => "PASSWORD_INVALID"
-            ];
+                        -> key('password', v::stringType()->length(8, 128)
+                        -> regex('/[A-Z]/')->regex('/[a-z]/')->regex('/[0-9]/')->regex('/[!@#*]/'), true);
 
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 여행 생성/ 여행 수정 유효성 검증
@@ -117,16 +74,8 @@
                         -> key('start_date', $this->vDateTimeRule(), true)
                         -> key('end_date', $this->vDateTimeRule(), true);
 
-            // 에러 배열
-            $map = [
-                'title' => "TITLE_INVALID",
-                'region_id' => "REGION_ID_INVALID",
-                'start_date' => "START_DATE_INVALID",
-                'end_date' => "END_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 일차 속성(메모) 수정 유효성 검증
@@ -134,13 +83,8 @@
             // memo 검증
             $validation = v::key('memo', $this->vLengthRule(null, 255), true);
 
-            // 에러 배열
-            $map = [
-                'memo' => "MEMO_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
         
         // 일차 생성 유효성 검증
@@ -149,14 +93,8 @@
             $validation = v::key('day_no', $this->vIntRule(), true)
                         -> key('memo', $this->vLengthRule(null, 255), true);
 
-            // 에러 배열
-            $map = [
-                'day_no' => "DAY_NO_INVALID",
-                'memo' => "MEMO_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 일차 재배치 유효성 검증
@@ -170,15 +108,8 @@
                 ), true
             );
 
-            // 에러 배열
-            $map = [
-                'orders' => "ORDERS_INVALID",
-                'day_no' => "DAY_NO_INVALID",
-                'new_day_no' => "NEW_DAY_NO_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         } 
 
         // 외부 결과를 내부로 저장 유효성 검증
@@ -187,25 +118,13 @@
                 -> key('name', $this->vStringRule(), true)
                 -> key('category', $this->vStringRule(), true)
                 -> key('address', $this->vStringRule(), true)
-                -> key('external_ref', $this->vStringRule(), true)
-                -> key('lat', $this->vFloatRule(), true)
-                -> key('lng', $this->vFloatRule(), true)
+                -> key('external_ref', v::stringType()->notOptional(), true)
+                -> key('lat', $this->vRequiredFloat(), true)
+                -> key('lng', $this->vRequiredFloat(), true)
                 -> key('url', $this->vStringRule(), false);
-            
-            // 에러 배열
-            $map = [
-                'place' => "PLACE_INVALID",
-                'name' => "NAME_INVALID",
-                'category' => "CATEGORY_INVALID",
-                'address' => "ADDRESS_INVALID",
-                'external_ref' => "EXTERNAL_REF_INVALID",
-                'lat' => "LAT_INVALID",
-                'lng' => "LNG_INVALID",
-                'url' => "URL_INVALID"
-            ];
 
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 일정 아이템 수정 유효성 검증
@@ -214,14 +133,8 @@
             $validation = v::key('visit_time', $this->vDateTimeRule(), true)
                     -> key('seq_no', $this->vIntRule(), true);
 
-            // 에러 배열
-            $map = [
-                'visit_time' => "VISIT_TIME_INVALID",
-                'seq_no' => "SEQ_NO_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 일정 아이템 추가 유형성 검증
@@ -230,16 +143,9 @@
             $validation = v::key('place_id', $this->vIntRule(), true)
                         -> key('visit_time', $this->vDateTimeRule(), true)
                         -> key('seq_no', $this->vIntRule(), true);
-            
-            // 에러 배열
-            $map = [
-                'place_id' => "PLACE_ID_INVALID",
-                'visit_time' => "VISIT_TIME_INVALID",
-                'seq_no' => "SEQ_NO_INVALID"
-            ];
 
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         // 일정 아이템 순서 재배치 유효성 검증
@@ -253,15 +159,8 @@
                 ), true
             );
 
-            // 에러 배열
-            $map = [
-                'orders' => "ORDERS_INVALID",
-                'item_id' => "ITEM_ID_INVALID",
-                'new_seq_no' => "NEW_SEQ_NO_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         /**  @param  */
@@ -269,11 +168,8 @@
             // 일정 아이템 id 검증
             $validation = $this->vIntRule();
 
-            // 에러 배열
-            $errorMap = [ 'item_id' => "ITEMID_INVALID" ];
-
             // try-catch 예외 처리 함수 실행
-            $this->paramValidation($validation, $data, $errorMap);
+            $this->runValidate($validation, $data);
         }
 
         /**  @param  */
@@ -281,11 +177,8 @@
             // 장소 id 검증
             $validation = $this->vIntRule();
 
-            // 에러 배열
-            $errorMap = [ 'place_id' => "PLACE_ID_INVALID" ];
-
             // try-catch 예외 처리 함수 실행
-            $this->paramValidation($validation, $data, $errorMap);
+            $this->runValidate($validation, $data);
         }
         
         /**  @param  */
@@ -293,11 +186,8 @@
             // 일차 id 검증
             $validation = $this->vIntRule();
 
-            // 에러 배열
-            $errorMap = [ 'day_no' => "DAY_NO_INVALID" ];
-
             // try-catch 예외 처리 함수 실행
-            $this->paramValidation($validation, $data, $errorMap);
+            $this->runValidate($validation, $data);
         }
 
         /**  @param  */
@@ -305,11 +195,8 @@
             // 여행 생성 id
             $validation = $this->vIntRule();
             
-            // 에러 배열
-            $errorMap = [ 'trip_id' => "TRIP_ID_INVALID" ];
-
             // try-catch 예외 처리 함수 실행
-            $this->paramValidation($validation, $data, $errorMap);
+            $this->runValidate($validation, $data);
         }
 
         // 지역 유효성 검증
@@ -317,11 +204,8 @@
         public function validateRegion($data) {
             $validation = $this->vIntRule();
             
-            // 에러 배열
-            $errorMap = [ 'region_id' => "REGION_ID_INVALID" ];
-
             // try-catch 예외 처리 함수 실행
-            $this->paramValidation($validation, $data, $errorMap);
+            $this->runValidate($validation, $data);
         }
 
 
@@ -329,14 +213,9 @@
         public function validateRegionSearch($data) {
             // 지역 이름 검증
             $validation = v::key('query', $this->vStringRule(), true);
-            
-            // 에러 배열
-            $map = [
-                'query' => "QUERY_INVALID"
-            ];
 
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         /**  @param 쿼리*/
@@ -345,23 +224,13 @@
             // 장소 검증
             $validation = v::key('place', $this->vStringRule(), false)
                 -> key('radius', $this->vStringRule(), false)
-                -> key('lat', $this->vFloatRule(), false)
-                -> key('lng', $this->vFloatRule(), false)
+                -> key('lat', $this->vRequiredFloat(), false)
+                -> key('lng', $this->vRequiredFloat(), false)
                 -> key('sort', $this->vStringRule(), false)
                 -> key('page', $this->vIntRule(), false);
 
-            // 에러 배열
-            $map = [
-                'place' => "PLACE_INVALID",
-                'radius' => "RADIUS_INVALID",
-                'lat' => "LAT_INVALID",
-                'lng' => "LNG_INVALID",
-                'sort' => "SORT_INVALID",
-                'page' => "PAGE_INVALID"
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
 
         /**  @param 쿼리*/
@@ -370,14 +239,8 @@
             $validation = v::key('page', $this->vIntRule(), false)
                         -> key('sort', $this->vStringRule(), false);
 
-            // 에러 배열
-            $map = [
-                'page' => "PAGE_INVALID",
-                'sort' => 'SORT_INVALID'
-            ];
-
             // try-catch 예외 처리 함수 실행
-            $this->runValidate($validation, $data, $map);
+            $this->runValidate($validation, $data);
         }
     }
 
