0060d99c0ae0b9f5ec4729b9eb3b17e7f8137db4
dayeon <dayeon2423004@g.yju.ac.kr>
2025-10-31 03:23:18 +0900

    서비스 수정

--- backend/app/Modules/Auth/Services/AuthService.php
+++ backend/app/Modules/Auth/Services/AuthService.php
@@ -1,53 +1,73 @@
 <?php
     namespace Tripmate\Backend\Modules\Auth\Services;
 
-    use Tripmate\Backend\Common\Utils\Password;
+    use Tripmate\Backend\Common\Exceptions\DbException;
+use Tripmate\Backend\Common\Exceptions\HttpException;
+use Tripmate\Backend\Common\Utils\Password;
+    use Tripmate\Backend\Core\Service;
+    use Tripmate\Backend\Core\DB;
     use Tripmate\Backend\Modules\Auth\Repositories\UserRepository;
     use Tripmate\Backend\Common\Middleware\AuthMiddleware as amw;
 
-    // 서비스 로직
-    class AuthService {
-        public UserRepository $repository;
+    /**
+     *  유저관리(회원가입, 로그인, 로그아웃) 서비스
+     */
+    class AuthService extends Service {
+        private UserRepository $repository;
 
-        // 생성자에서 DB 호출
+        /**
+         * 생성자 호출
+         * - 부모 생성자 PDO 주입 및 리포지토리 초기화
+         */
         public function __construct() {
-            $this->repository = new UserRepository();
+            parent::__construct(DB::conn());
+            $this->repository = new UserRepository($this->db);
         }
 
-        // 회원가입 로직
-        public function registerServices(array $data) {
-            // 데이터 꺼내기
-            $email = $data['email'];
-            $password = $data['password'];
-            $nickname = $data['nickname'];
+        /**
+         * 회원가입 서비스
+         * - 비밀번호 해쉬 후 DB 저장 호출
+         * @param array $data
+         * @return string
+         */
+        public function registerUser(array $data) {
 
-            // 비밀번호 해쉬화
-            $pwdHash = Password::passwordHash($password);
+            return $this->transaction(function() use ($data) {
+                try {
+                    $email = $data['email'];
+                    $password = $data['password'];
+                    $nickname = $data['nickname'];
 
-            // 이메일 정규화
-            $emailNorm = strtolower($email);
-            
-            // DB 실행
-            $result = $this->repository->registerDB($emailNorm, $pwdHash, $nickname);
+                    $hashedPassword = Password::hash($password);
 
-            return $result;
-        }
-
-        // 로그인
-        public function loginServices($data) {
-            // 데이터 확인
-            $email = $data['email'];
-            $pwd = $data['password'];
-
-            // DB 실행
-            $result = $this->repository->loginDB($email, $pwd);
-
-            if (is_int($result)) { 
-                $jwt = amw::tokenRequest($result);
-                    return $jwt;
+                    $normalizedEmail = strtolower($email); // 이메일 정규화
+                    
+                    return $this->repository->createUser($normalizedEmail, $hashedPassword, $nickname);
+                } catch (DbException $e) {
+                    switch ($e->getCode()) {
+                        case 'USER_DUPLICATE_EMAIL':
+                            throw new HttpException(500, $e->getMessage(), $e->getCode());
+                        case 'UNEXPECTED_ERROR':
+                            throw new HttpException(500, $e->getMessage(), $e->getCode());
+                    }
                 }
+            });
+        }
 
-            // jwt 외 에러 반환
-            return $result;
+        /**
+         * 로그인 서비스
+         * - DB 검증 후 토큰 값 반환
+         * @param array $data
+         * @return string 
+         */
+        public function loginUser($data) {
+            return $this->transaction(function() use ($data) {
+                $email = $data['email'];
+                $password = $data['password'];
+
+                $result = $this->repository->loginDB($email, $password);
+
+                return amw::tokenRequest($result);
+            });
         }
     }
