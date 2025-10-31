28d3b98354fc639d66c9f5fc88fd12ddb54b4c8c
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-01 03:00:47 +0900

    라우터 수정

--- backend/app/Modules/Users/Routes.php
+++ backend/app/Modules/Users/Routes.php
@@ -6,8 +6,8 @@
     // User 라우트 등록
     return function (\AltoRouter $router, Request $request, Response $response): void {
         //  라우팅 등록
-        $router->map('GET', '/api/v1/users/me', [new UsersController($request, $response), 'userMyPage']);
+        $router->map('GET', '/api/v1/users/me', [UsersController::class, 'userMyPage']);
 
         //  라우팅 등록
-        $router->map('DELETE', '/api/v1/users/me', [new UsersController($request, $response), 'userSecession']);
+        $router->map('DELETE', '/api/v1/users/me', [UsersController::class, 'userSecession']);
     };
