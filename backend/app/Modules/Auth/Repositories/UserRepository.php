4a6cf12d0a4cc556fe91d007245a7acd60375311
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-01 02:33:40 +0900

    리포지토 리펙토링

--- backend/app/Modules/Auth/Repositories/UserRepository.php
+++ backend/app/Modules/Auth/Repositories/UserRepository.php
@@ -15,66 +15,40 @@
             parent::__construct($db);
         }
 
-        /**
-         * 회원가입 리포지토리
-         * - 
-         * @param string $normalizedEmail
-         * @param string $hashedPassword
-         * @param string $nickname
-         */
+        // 이메일 중복 검증 로직
+        public function findEmail($normalizedEmail) {
+            $selectSql = "SELECT email_norm FROM Users WHERE email_norm = :email";
+            $selectParm = ["email" => $normalizedEmail];
+            return $this->execute($selectSql, $selectParm);
+        }
+
+        // 회원 저장 로직 
         public function createUser(string $normalizedEmail, string $hashedPassword, string $nickname) {
-
-            return DB::transaction(function() use ($normalizedEmail, $hashedPassword, $nickname) {
-                try {
-                    // email 중복 확인
-                    $selectSql = "SELECT email_norm FROM Users WHERE email_norm = :email";
-                    $selectParm = ["email" => $normalizedEmail];
-                    $result = $this->fetchOne($selectSql, $selectParm);
-
-                    // 값이 존재할 경우
-                    if ($result) {
-                        throw new DbException(500,"USER_DUPLICATE_EMAIL", "이미 사용중인 이메일입니다.");
-                    }
-
-                    // 유저 생성
-                    $insertSql = "INSERT INTO Users (email_norm, password_hash, name) VALUES (:email_norm, :password_hash, :name);";
-                    $insertParm = ['email_norm' => $normalizedEmail, 'password_hash' => $hashedPassword, 'name' => $nickname];
-                    $this->query($insertSql, $insertParm);
-                
-                } catch (\Throwable $e) {
-                    throw new DbException("UNEXPECTED_ERROR", "회원가입 처리 중 알 수 없는 오류가 발생했습니다.", $e);
-                }
-            });
+            // 유저 생성
+            $insertSql = "INSERT INTO Users (email_norm, password_hash, name) VALUES (:email_norm, :password_hash, :name);";
+            $insertParm = ['email_norm' => $normalizedEmail, 'password_hash' => $hashedPassword, 'name' => $nickname];
+            $this->query($insertSql, $insertParm);
         }
 
         // 로그인 로직
-        public function loginDB($email, $password) {
-            return DB::transaction(function() use ( $email, $password ) {
-                try {
-                    $selectSql = "SELECT user_id, password_hash FROM Users WHERE email_norm = ?;";
-                    $selectParm = ["email"=> $email]; 
-                    $data = $this->fetchOne($selectSql, $selectParm);
+        public function findUser($email, $password) {
+            $selectSql = "SELECT user_id, password_hash FROM Users WHERE email_norm = :email;";
+            $selectParm = ["email"=> $email]; 
+            $data = $this->fetchOne($selectSql, $selectParm);
 
-                    // 이메일 조회 반환 값이 없을 경우
-                    if(!$data) {
-                        throw new DbException(500,"EMAIL", "있다");
-                    }
+            // 이메일 조회 반환 값이 없을 경우
+            if(!$data) {
+                throw new DbException("NOT_EMAIL_FOUND","등록되지 않은 이메일입니다.");
+            }
+            $userId = $data['user_id'];
+            $pwdHash = $data['password_hash'];
+            error_log($userId);
 
-                    // 조회한 데이터 꺼내기
-                    $userId = $data['user_id'];
-                    $pwdHash = $data['password_hash'];
+            // 비밀번호 검증
+            if((Password::verify($password, $pwdHash)) === false) {
+                throw new DbException("PASSWORD_NOT",'패스워드 불일치로 로그인에 실패하였습니다.');
+            }
 
-                    // 비밀번호 검증
-                    if(Password::verify($password, $pwdHash)) {
-                        // JWT 발급
-                        return $userId;
-                    } else {
-                        // 비밀번호가 알맞지 않을 경우
-                        throw new DbException(500,'PASSWORD','');
-                    }
-                } catch (\Throwable $e) {
-                    throw new DbException('user', '에러', $e);
-                }
-            });
+            return $userId;
         }
     }
