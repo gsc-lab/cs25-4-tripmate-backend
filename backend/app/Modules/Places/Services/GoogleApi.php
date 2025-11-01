2b1df57122d96244b30bfee23c53c0ebf21e5125
dayeon <dayeon2423004@g.yju.ac.kr>
2025-11-02 03:33:00 +0900

    api 수정

--- backend/app/Modules/Places/Services/GoogleApi.php
+++ backend/app/Modules/Places/Services/GoogleApi.php
@@ -4,17 +4,46 @@
     use Tripmate\Backend\Common\Exceptions\HttpException;
 
     class GoogleApi {
-        // google 공통 API 연결 함수
-        public static function searchService($endpoint, $headers, $postData) {
+        // API_KEY 함수
+        private static function apiKey() {
+            return $_ENV['API_KEY'] ?? getenv('API_KEY');
+        }
 
+        // POST 요청 함수
+        public static function post($endpoint, $postData = [], $headers = []) {
+            $defaultHeaders = ['Content-Type: application/json',
+                            'X-Goog-Api-Key:' . self::apiKey()];
+
+            $mergeHeader = array_merge($headers, $defaultHeaders);
+
+            return self::searchService($endpoint, "POST", $mergeHeader, $postData);
+        }
+
+        // GET 요청 함수
+        public static function get ($endpoint, $params = []) {
+            $params['key'] = self::apiKey();
+            $params['language'] = "ko";
+
+            $url = $endpoint . '?' . http_build_query($params);
+
+            return self::searchService($url, "GET");
+        }
+
+
+        // google 공통 API 연결 함수
+        public static function searchService($endpoint, $method = "GET", $headers = [], $postData = null) {
             // 서버 전달
             $curl = curl_init($endpoint); // API 주소 연결
-            curl_setopt($curl, CURLOPT_POST, true); // 주소 옵션 : POST, 기본값 : GET
-            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // 헤더 전달
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postData)); // 본문 전달
+
+            if ($method = "POST") {
+                curl_setopt($curl, CURLOPT_POST, true); // 주소 옵션 : POST, 기본값 : GET
+                curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // 헤더 전달
+                curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($postData)); // 본문 전달   
+            }
+
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); // SSL 인증서 확인(true 보안적)
-            
             curl_setopt($curl, CURLOPT_RETURNTRANSFER, true); // 실행 결과를 echo하지 말고 변수로 반환
+            
             $response = curl_exec($curl); // 실행
 
             $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE); // HTTP 상태 코드
@@ -22,9 +51,9 @@
 
             curl_close($curl);
 
-            // 응답 처리
-            if ($httpCode !== 200 || $response === false) {
-                throw new HttpException("500", "API_ERROR", "외부 API 연결에 실패했습니다.");
+            // 에러 처리
+            if ($httpCode >= 400 || $response === false) {
+                throw new HttpException("500", "GOOGLE_API_FAILED", "외부 API 호출에 실패했습니다.", $error);
             }
 
             // 데이터 본문 처리
