a2802528ba1905854a65d90ff4859d0dd68275b2
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2025-11-02 12:28:32 +0900

    Refactor/feature register (#95)
    * authController 리펙토링
    
    * controller 수정
    
    * 메서드명 수정
    
    * 라우터 수정
    
    * 호출 메서드 수정
    
    * 리포지 수정
    
    * 서비스 수정
    
    * 라우터 클래스 정적 호출
    
    * 컨트롤러 리펙토링
    
    * routes 경로 수정
    
    * 리포지토 리펙토링
    
    * 서비스 리펙토링
    
    * 컨트롤러 수정
    
    * Remove error log on user creation failure
    
    Removed error logging for user creation failure.

--- backend/app/Modules/Auth/Services/AuthService.php
+++ backend/app/Modules/Auth/Services/AuthService.php
@@ -1,53 +1,79 @@
 <?php
     namespace Tripmate\Backend\Modules\Auth\Services;
 
+    use Tripmate\Backend\Common\Exceptions\DbException;
+    use Tripmate\Backend\Common\Exceptions\HttpException;   
     use Tripmate\Backend\Common\Utils\Password;
+    use Tripmate\Backend\Core\Service;
+    use Tripmate\Backend\Core\DB;
     use Tripmate\Backend\Modules\Auth\Repositories\UserRepository;
     use Tripmate\Backend\Common\Middleware\AuthMiddleware as amw;
 
-    // 서비스 로직
-    class AuthService {
-        public UserRepository $repository;
+    /**
+     *  유저관리(회원가입, 로그인, 로그아웃) 서비스
+     */
+    class AuthService extends Service {
+        private UserRepository $repository;
 
-        // 생성자에서 DB 호출
+        /**
+         * 생성자 호출
+         * - 부모 생성자 PDO 주입 및 리포지토리 초기화
+         */
         public function __construct() {
-            $this->repository = new UserRepository();
+            parent::__construct(DB::conn());
+            $this->repository = new UserRepository($this->db);
         }
 
-        // 회원가입 로직
-        public function registerServices(array $data) {
-            // 데이터 꺼내기
-            $email = $data['email'];
-            $password = $data['password'];
-            $nickname = $data['nickname'];
+        /**
+         * 회원가입 서비스
+         * - 비밀번호 해쉬 후 DB 저장 호출
+         * @param array $data
+         * @return string
+         */
+        public function registerUser(array $data) {
 
-            // 비밀번호 해쉬화
-            $pwdHash = Password::passwordHash($password);
+            try {   
+                return $this->transaction(function() use ($data) {
+                    $email = $data['email'];
+                    $password = $data['password'];
+                    $nickname = $data['nickname'];
 
-            // 이메일 정규화
-            $emailNorm = strtolower($email);
-            
-            // DB 실행
-            $result = $this->repository->registerDB($emailNorm, $pwdHash, $nickname);
+                    // 이메일 중복 확인
+                    $normalizedEmail = strtolower($email); // 이메일 정규화
+                    $result = $this->repository->findEmail($normalizedEmail);
+                    if ($result) {
+                        throw new HttpException(409, 'USER_DUPLICATE_EMAIL', '이미 사용중인 이메일입니다.');
+                    }
 
-            return $result;
-        }
+                    // 회원 데이터 저장
+                    $hashedPassword = Password::hash($password);
+                    return $this->repository->createUser($normalizedEmail, $hashedPassword, $nickname);
+                    });
 
-        // 로그인
-        public function loginServices($data) {
-            // 데이터 확인
-            $email = $data['email'];
-            $pwd = $data['password'];
-
-            // DB 실행
-            $result = $this->repository->loginDB($email, $pwd);
-
-            if (is_int($result)) { 
-                $jwt = amw::tokenRequest($result);
-                    return $jwt;
+                } catch (DbException $e) {
+                    throw new HttpException(500, 'UNEXPECTED_ERROR', "회원가입 중 알 수 없는 에러가 발생하였습니다.", $e);
                 }
+            }
 
-            // jwt 외 에러 반환
-            return $result;
+        /**
+         * 로그인 서비스
+         * - DB 검증 후 토큰 값 반환
+         * @param array $data
+         * @return string 
+         */
+        public function loginUser($data) {
+            try {
+                return $this->transaction(function() use ($data) {
+                    $email = $data['email'];
+                    $password = $data['password'];
+
+                    // 유저 검증
+                    $userId = $this->repository->findUser($email, $password);
+
+                    return amw::tokenRequest($userId);
+                });
+            } catch (DbException $e) {
+                throw new HttpException(500, 'LOGIN_ERROR', '로그인 도중 알 수 없는 에러가 발생하였습니다.', $e);
+            }
         }
     }
