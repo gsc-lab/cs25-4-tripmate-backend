4059e06c6638f2a55a74a3ec24e44a8e6f66dee9
dayeon <163419727+dayeon2423004@users.noreply.github.com>
2025-11-02 12:29:38 +0900

    fix: Refactor/fix regions (#97)
    * 라우터 파일 수정
    
    * 시드 수정
    
    * 컨트롤러 수정
    
    * 서비스 수정
    
    * 리포지토리 수정"

--- backend/app/Modules/Regions/Controllers/RegionsController.php
+++ backend/app/Modules/Regions/Controllers/RegionsController.php
@@ -5,43 +5,32 @@
     use Tripmate\Backend\Modules\Regions\Services\RegionsService;
     use Tripmate\Backend\Core\Validator;
 
+    /**
+     * 지역 컨트롤러(지역 검색)
+     */
     class RegionsController extends Controller{
-        public RegionsService $service;
-        public Validator $validator;
+        private RegionsService $service;
+        private Validator $validator;
 
-        // 생성자
         public function __construct($request, $response) {
-            // 부모 컨트롤러 생성자
             parent::__construct($request, $response);
-            
-            // 서비스 생성자
             $this->service = new RegionsService();
-
-            // 유효성 검증
             $this->validator = new Validator();
         }
 
-        // 지역 검색
+        /**
+         * 지역 검색 컨트롤러
+         * 
+         */
         public function regionSearch() {
-            // 데이터 꺼내기
-            $query = $this->request->query['query'];
+            return $this->run(function() {
+                $region = $this->request->query(); // 지역 이름 데이터
+                $this->validator->validateRegionSearch($region);
+                $region = $region['query'];
 
-            // 유효성 검증
-            $result = $this->validator->validateRegionSearch($query);
-
-            if($result === false) {
-                $this->error($result, "지역 값이 존재하지 않거나 알맞지 않습니다.");
-                exit;
-            }
-
-            // 서비스 호출
-            $regionService = $this->service->regionService($query);
-        
-            // 출력
-            if($regionService == "DB_EXCEPTION") {
-                $this->error($regionService, "지역 조회에 실패하였습니다.");
-            } else {
-                $this->success($regionService);
-            }
+                $result = $this->service->regionMatch($region);
+            
+                return $result;
+            });
         }
     }
